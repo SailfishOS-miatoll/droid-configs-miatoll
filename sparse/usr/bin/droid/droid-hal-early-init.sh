c67727b107e1835154d44c753adc5aff3224b34a
AntonIX <antonix@devpins.org>
2024-03-22 22:55:48 +0200

    Files moved to droid-system

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 mv /system /system_root
-mkdir -p /system /vendor /product /system_ext /metadata
+mkdir -p /system /vendor /product /system_ext /metadata /odm
 
 # Binding mountpoints to sailfish used directories.
 
@@ -9,6 +9,7 @@ mount -o bind /system_root/system /system
 mount -o bind /system_root/vendor /vendor
 mount -o bind /system_root/system_ext /system_ext
 mount -o bind /system_root/product /product
+mount -o bind /vendor/odm /odm
 mount -o bind /odm /system_root/odm
 
 mount -o noatime,nosuid,nodev,discard    /dev/sda12   /metadata
@@ -19,11 +20,6 @@ mount -o ro,shortname=lower,uid=1000,gid=1000,dmask=227,fmask=337  /dev/sde4    
 mount -o ro,shortname=lower,uid=1002,gid=3002,dmask=227,fmask=337  /dev/sde5       /vendor/bt_firmware
 
 # Sound fix.
-mount --bind /etc/audio_policy_configuration.xml /vendor/etc/audio_policy_configuration.xml
 ln -s /usr/lib64/pulse-14.2/modules/libdroid-util.so /usr/lib64/pulseaudio/libdroid-util.so
 
-
-# Fixed jail properties for HW codecs.
-# mount --bind /etc/codec2.vendor.base.policy /vendor/etc/seccomp_policy/codec2.vendor.base.policy
-#
 sh /usr/bin/droid/stune-fix.sh
