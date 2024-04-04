831f17cd8eef2242d24e131ea644abaefada21e8
AntonIX <antonix@devpins.org>
2024-04-04 23:11:09 +0300

    Try deep sleed

--- sparse/usr/bin/droid/droid-hal-startup.sh
+++ sparse/usr/bin/droid/droid-hal-startup.sh
@@ -11,6 +11,8 @@ echo $NOTIFY_SOCKET > /run/droid-hal/notify-socket-name
 echo 1 > /sys/kernel/boot_adsp/boot
 echo 1 > /sys/kernel/boot_cdsp/boot
 
+echo deep > /sys/power/mem_sleep
+
 # Use exec nohup since systemd may send SIGHUP, but droid-hal-init doesn't
 # handle it. This avoids having to modify android_system_core, which would
 # require different handling for every different android version.
